de318b3ad20ed4bb9c3deb7e37240a7f419d7ead
Chris Hennighan <chennighan@q4613609hm.lan>
2023-02-13 18:38:25 -0500

    counts are wrong, table column is implemented. The logic is off I need to fix it.

--- dist/index.js
+++ dist/index.js
@@ -16437,6 +16437,10 @@ module.exports = require("path");
 /***/ (function(module) {
 
 module.exports = (pulls, authorId) => {
+  /*
+   TODO: the counts are not adding up to what was correct before.
+   Table is implemented with the addition of PR Count but numbers are wrong
+   */
   const count = pulls.filter((pull) => pull.author.id === authorId).length;
   return count;
 };
--- src/interactors/getTotalPrsByUser/index.js
+++ src/interactors/getTotalPrsByUser/index.js
@@ -1,4 +1,8 @@
 module.exports = (pulls, authorId) => {
+  /*
+   TODO: the counts are not adding up to what was correct before.
+   Table is implemented with the addition of PR Count but numbers are wrong
+   */
   const count = pulls.filter((pull) => pull.author.id === authorId).length;
   return count;
 };
