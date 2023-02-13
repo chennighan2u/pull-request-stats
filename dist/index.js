8b7f82b5e19e55ea444ede2ad865248ca3045f1a
Chris Hennighan <chennighan@q4613609hm.lan>
2023-02-13 18:22:24 -0500

    updating the constants to include total prs

--- dist/index.js
+++ dist/index.js
@@ -16489,15 +16489,17 @@ const SORT_KEY = {
   TIME: 'timeToReview',
   REVIEWS: 'totalReviews',
   COMMENTS: 'totalComments',
+  PRS: 'totalPRs',
 };
 
-const COLUMNS_ORDER = ['totalReviews', 'timeToReview', 'totalComments'];
+const COLUMNS_ORDER = ['totalReviews', 'timeToReview', 'totalComments', 'totalPrs'];
 
 const STATS_OPTIMIZATION = {
   totalReviews: 'MAX',
   totalComments: 'MAX',
   commentsPerReview: 'MAX',
   timeToReview: 'MIN',
+  totalPrs: 'MAX',
 };
 
 const STATS = Object.keys(STATS_OPTIMIZATION);
